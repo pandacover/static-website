4f115da5bca93e298e32f4fa4d4c2b9cd0e673fc
luvmakin01 <57311008+luvmakin01@users.noreply.github.com>
2021-06-06 00:39:32 +0530

    idk what to type bruv

---
+++ script.js
@@ -0,0 +1,10 @@
+const buttonClick = () => {
+  const menu_icon = document.querySelector('#menu-icon');
+  const navbar_acitve = document.querySelector('.navbar-nav');
+
+  menu_icon.addEventListener('click', () => {
+    menu_icon.classList.toggle('fa-times')
+    navbar_acitve.classList.toggle('navbar-active')
+    });
+  }
+buttonClick();
